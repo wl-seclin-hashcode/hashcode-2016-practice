cd7aa190518ba411995e9c0b00464656c70010a5
Thibaut Robert <thibaut.robert@worldline.com>
2015-11-17 14:20:24 +0100

    fix bug on erasecell optimize full squares

--- src/painting/Solver.scala
+++ src/painting/Solver.scala
@@ -9,10 +9,15 @@ import painting.Problem.{picture, nrow, ncol}
  * Time: 12:36
  */
 object Solver {
-  def solve = paintArea(5)
+  def solve = { paintArea(51, 8, false) ++ paintArea(5, 0, true) }
 
-  def paintArea(areaSize: Int): IndexedSeq[Command] = {
-    if (areaSize <= 0) IndexedSeq.empty[Command]
+  def paintArea(areaSize: Int, stop:Int, notFull: Boolean): IndexedSeq[Command] = {
+    def shouldPaint(area: Array[String]) = if (notFull)
+      area.map(_.count('#'.==)).sum > area.map(_.count('.'.==)).sum
+    else
+      area.forall(_.forall('#'.==))
+
+    if (areaSize <= stop ) IndexedSeq.empty[Command]
     else {
       val r = for {
         row ← 0 until nrow by areaSize
@@ -20,18 +25,18 @@ object Solver {
       } yield {
         val area = picture.slice(row, row + areaSize) map (_.slice(col, col + areaSize))
 
-        if (area.map(_.count('#'.==)).sum > area.map(_.count('.'.==)).sum && area.length == area.head.length) {
+        if (shouldPaint(area) && area.length == area.head.length) {
           val erases = for {r1 ← area.indices
                             c1 ← area(r1).indices
                             if area(r1)(c1) == '.'
-          } yield Erase(r1, c1)
+          } yield Erase(row + r1, row + c1)
           for {r1 ← row until row + area.length
                c1 ← col until col + area.head.length}
             picture(r1) = picture(r1).updated(c1, '.')
           Paint(row + area.length/2, col + area.length/2 , area.length/2) +: erases
         } else IndexedSeq.empty[Command]
       }
-      r.flatten ++ paintArea(areaSize - 2)
+      r.flatten ++ paintArea(areaSize - 2, stop, notFull)
     }
   }
 }
