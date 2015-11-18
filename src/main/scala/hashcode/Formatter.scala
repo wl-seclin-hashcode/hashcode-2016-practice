5ff3833b66cd8316ddb225d453ae1646500ff7b0
Michel Daviot <michel@daviot.info>
2015-11-18 21:15:45 +0100

    fix out format

--- src/main/scala/hashcode/Formatter.scala
+++ src/main/scala/hashcode/Formatter.scala
@@ -6,7 +6,7 @@ object Formatter {
   def write(solution: Solution, score: Int): Unit = {
     val name = s"out.${score}.txt"
     val f = new PrintStream(name)
-    f.println(solution.toString)
+    f.println(solution.commands.mkString("\n"))
     f.close
     println(s"wrote to $name")
   }
--- src/main/scala/hashcode/Solver.scala
+++ src/main/scala/hashcode/Solver.scala
@@ -1,41 +1,45 @@
 package hashcode
 
 object Solver {
-  def solve(problem: Problem): Solution =
-    {
-      import problem._
-      def paintArea(areaSize: Int, stop: Int, notFull: Boolean): IndexedSeq[Command] = {
-        def shouldPaint(area: Array[String]) = if (notFull)
+  def solve(problem: Problem): Solution = {
+    import problem._
+
+    def paintArea(areaSize: Int, stop: Int, notFull: Boolean): IndexedSeq[Command] = {
+      def shouldPaint(area: Array[String]) =
+        if (notFull)
           area.map(_.count('#'.==)).sum > area.map(_.count('.'.==)).sum
         else
           area.forall(_.forall('#'.==))
 
-        if (areaSize <= stop) IndexedSeq.empty[Command]
-        else {
-          val r = for {
-            row ← 0 until nrow by areaSize
-            col ← 0 until ncol by areaSize
-          } yield {
-            val area = picture.slice(row, row + areaSize) map (_.slice(col, col + areaSize))
+      if (areaSize <= stop) IndexedSeq.empty[Command]
+      else {
+        val r = for {
+          row ← 0 until nrow by areaSize
+          col ← 0 until ncol by areaSize
+        } yield {
+          val area = picture.slice(row, row + areaSize) map (_.slice(col, col + areaSize))
 
-            if (shouldPaint(area) && area.length == area.head.length) {
-              val erases = for {
-                r1 ← area.indices
-                c1 ← area(r1).indices
-                if area(r1)(c1) == '.'
-              } yield Erase(row + r1, col + c1)
-              for {
-                r1 ← area.indices
-                c1 ← area(r1).indices
-                if area(r1)(c1) == '#'
-              } picture(row + r1) = picture(row + r1).updated(col + c1, '0')
-              Paint(row + area.length / 2, col + area.length / 2, area.length / 2) +: erases
-            } else IndexedSeq.empty[Command]
-          }
-          r.flatten ++ paintArea(areaSize - 2, stop, notFull)
+          if (shouldPaint(area) && area.length == area.head.length) {
+            val erases = for {
+              r1 ← area.indices
+              c1 ← area(r1).indices
+              if area(r1)(c1) == '.'
+            } yield Erase(row + r1, col + c1)
+
+            for {
+              r1 ← area.indices
+              c1 ← area(r1).indices
+              if area(r1)(c1) == '#'
+            } picture(row + r1) = picture(row + r1).updated(col + c1, '0')
+
+            Paint(row + area.length / 2, col + area.length / 2, area.length / 2) +: erases
+          } else IndexedSeq.empty[Command]
         }
+        r.flatten ++ paintArea(areaSize - 2, stop, notFull)
       }
-      Solution(paintArea(51, 6, false) ++ paintArea(5, 0, true))
     }
+
+    Solution(paintArea(51, 6, false) ++ paintArea(5, 0, true))
+  }
 
 }
