022d1dc67df4cac8e4e5f5c8a171c6bcf47cbf60
Michel Daviot <michel@daviot.info>
2016-02-05 22:29:55 +0100

    [chore] align scalariform configuration with default eclipse behaviour

--- src/main/scala/hashcode/Formatter.scala
+++ src/main/scala/hashcode/Formatter.scala
@@ -3,8 +3,8 @@ package hashcode
 import java.io.PrintStream
 
 object Formatter {
-  def write(solution: Solution, score: Int, name:String): Unit = {
-    val file=s"out.$name.$score.txt"
+  def write(solution: Solution, score: Int, name: String): Unit = {
+    val file = s"out.$name.$score.txt"
     val f = new PrintStream(file)
     f.println(solution.commands.size)
     f.println(solution.commands.mkString("\n"))
--- src/main/scala/hashcode/Parser.scala
+++ src/main/scala/hashcode/Parser.scala
@@ -4,7 +4,7 @@ import java.io.File
 import java.util.Scanner
 
 object Parser {
-  def read(f:String): Problem = {
+  def read(f: String): Problem = {
     val scan = new Scanner(new File(f))
     import scan._
     val nrow = nextInt()
