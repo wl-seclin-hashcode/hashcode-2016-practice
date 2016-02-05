c9c164c7cd35ceac01427569ee6c74253f70836d
Michel Daviot <michel@daviot.info>
2016-02-05 22:09:38 +0100

    fail fast when the solution is invalid (out of the area)

--- src/main/scala/hashcode/Solution.scala
+++ src/main/scala/hashcode/Solution.scala
@@ -5,20 +5,22 @@ import scala.collection.IndexedSeq
 case class Solution(commands: IndexedSeq[Command])
 
 trait Command {
-  def update(picture: Map[(Int, Int), Boolean]): Map[(Int, Int), Boolean]
+  def update(picture: Map[(Int, Int), Boolean], problem: Problem): Map[(Int, Int), Boolean]
 }
 
 case class Paint(row: Int, col: Int, size: Int) extends Command {
+
   override def toString = s"PAINT_SQUARE $row $col $size"
-  def update(picture: Map[(Int, Int), Boolean]): Map[(Int, Int), Boolean] = {
+
+  def update(picture: Map[(Int, Int), Boolean], problem: Problem): Map[(Int, Int), Boolean] = {
     val row0 = row - size
     val col0 = col - size
     assert(row0 >= 0)
     assert(col0 >= 0)
     val rown = row + size
     val coln = col + size
-    //    assert(rown < Problem.nrow)
-    //    assert(coln < Problem.ncol)
+    assert(rown < problem.nrow, s"row $rown")
+    assert(coln < problem.ncol, s"col $coln")
     val area = for (i <- row0 to rown; j <- col0 to coln) yield (i, j)
     area.foldLeft(picture) {
       case (pic, (i, j)) => pic.updated((i, j), true)
@@ -28,6 +30,9 @@ case class Paint(row: Int, col: Int, size: Int) extends Command {
 case class Erase(row: Int, col: Int) extends Command {
   override def toString = s"ERASE_CELL $row $col"
 
-  def update(picture: Map[(Int, Int), Boolean]): Map[(Int, Int), Boolean] =
+  def update(picture: Map[(Int, Int), Boolean], problem: Problem): Map[(Int, Int), Boolean] = {
+    assert(row < problem.nrow)
+    assert(col < problem.ncol)
     picture.updated((row, col), false)
+  }
 }
--- src/main/scala/hashcode/Validator.scala
+++ src/main/scala/hashcode/Validator.scala
@@ -7,29 +7,31 @@ import scala.util.Try
 object Validator {
   def score(solution: Solution, problem: Problem): Try[Int] = {
     Try {
-      validate(solution.commands.toList, problem.picture)
+      validate(solution.commands.toList, problem)
       solution.commands.size
     }
   }
 
-  def validate(sol: List[Command], picture: Array[String]) {
-    val painted = paintRec(sol, Map.empty.withDefaultValue(false))
+  def validate(sol: List[Command], problem: Problem) {
+    val painted = paintRec(sol, Map.empty.withDefaultValue(false), problem)
 
     for {
-      i <- 0 until picture.length
-      line = picture(i)
-      j <- 0 until line.length
+      i <- 0 until problem.nrow
+      line = problem.picture(i)
+      j <- 0 until problem.ncol
       c = line(j)
       shouldPaint = c == '#'
       isPainted = painted((i, j))
       ok = isPainted == shouldPaint
-      //      _ = println(i, j, c, shouldPaint, ok)
     } assert(ok, s"($i,$j) should be $shouldPaint but was $isPainted ($c)")
   }
 
   @tailrec
-  def paintRec(sol: List[Command], picture: Map[(Int, Int), Boolean]): Map[(Int, Int), Boolean] = sol match {
-    case Nil => picture
-    case command :: tail => paintRec(tail, command.update(picture))
+  def paintRec(
+    sol: List[Command],
+    picture: Map[(Int, Int), Boolean],
+    problem: Problem): Map[(Int, Int), Boolean] = sol match {
+    case Nil             => picture
+    case command :: tail => paintRec(tail, command.update(picture, problem), problem)
   }
 }
