576b8aab090a7f28361454e26c147cd9a6438e3a
Michel Daviot <michel@daviot.info>
2016-02-06 13:52:31 +0100

    display total score

--- src/main/scala/hashcode/Main.scala
+++ src/main/scala/hashcode/Main.scala
@@ -6,18 +6,16 @@ import scala.util.Success
 
 object Main extends App {
   val names = Seq("learn_and_teach.in", "logo.in", "right_angle.in")
-  names.foreach(solveIt)
+  println(s"total score for $names : " + names.map(solveIt).sum)
 
   def solveIt(n: String) = {
     val problem = Parser.read(n)
     val solution = Solver.solve(problem)
-    Validator.score(solution, problem) match {
-      case Success(score) =>
-        println(s"score for $n : $score")
-        Formatter.write(solution, score, n)
-      case Failure(e) =>
-        e.printStackTrace()
-    }
+    Validator.score(solution, problem).recover { case e => e.printStackTrace(); 0 }.map { score =>
+      println(s"score for $n : $score")
+      Formatter.write(solution, score, n)
+      score
+    }.get
   }
 
 }
