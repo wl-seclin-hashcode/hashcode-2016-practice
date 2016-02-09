962ba1f3c428825353bbac03abcf2e1f382a73a3
Michel Daviot <michel@daviot.info>
2016-02-09 22:52:13 +0100

    remove dead code

--- src/main/scala/hashcode/Problem.scala
+++ src/main/scala/hashcode/Problem.scala
@@ -48,30 +48,21 @@ case class Shape(points: Set[Point]) {
   val maxRow = points.maxBy(_.row).row
   val maxCol = points.maxBy(_.col).col
 
-  def lines = for {
+  lazy val lines = for {
     (_, pts) ← points.groupBy(_.row).toList.sortBy(_._1)
     line ← groups { p ⇒ p.copy(col = p.col + 1) }(pts.toList.sortBy(_.col))
   } yield line
 
-  def cols = for {
+  lazy val cols = for {
     (_, pts) ← points.groupBy(_.col).toList.sortBy(_._1)
     col ← groups { p ⇒ p.copy(row = p.row + 1) }(pts.toList.sortBy(_.row))
   } yield col
 
-  def groups(next: Point ⇒ Point)(pts: List[Point], group: List[Point] = Nil, acc: List[List[Point]] = Nil): List[List[Point]] = pts match {
+  private def groups(next: Point ⇒ Point)(pts: List[Point], group: List[Point] = Nil, acc: List[List[Point]] = Nil): List[List[Point]] = pts match {
     case Nil ⇒ if (group.isEmpty) acc else group :: acc
     case h :: t ⇒
       if (group.isEmpty || h == next(group.head)) groups(next)(t, h :: group, acc)
       else groups(next)(t, List(h), group :: acc)
-  }
-
-  def image: IndexedSeq[IndexedSeq[Boolean]] = {
-    for {
-      row ← minRow to maxRow
-    } yield for {
-      col ← minCol to maxCol
-      point = Point(row, col)
-    } yield points.contains(point)
   }
 }
 
